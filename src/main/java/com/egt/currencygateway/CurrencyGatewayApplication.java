13008c2c79d20374553c64e8084e7fc0d744276d
Zahari Zvezdomirov <zvezdomirov@Zaharis-MacBook-Air.local>
2024-09-18 09:56:55 +0300

    Initial commit: add project setup/configuration

---
+++ src/main/java/com/egt/currencygateway/CurrencyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.egt.currencygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyGatewayApplication.class, args);
+	}
+
+}
